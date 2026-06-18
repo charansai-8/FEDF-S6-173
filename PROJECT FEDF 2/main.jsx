95d04a7cdd9995d876362c806ea3dc69467c81e4
charansai-8 <yerrapragadacharansai@gmail.com>
2026-06-18 20:18:58 +0530

    Add files via upload

---
+++ PROJECT FEDF 2/main.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+
+// Import Bootstrap 5 CSS & JS bundle
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+// Import Bootstrap Icons
+import 'bootstrap-icons/font/bootstrap-icons.css'
+// Import Custom Global CSS
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
